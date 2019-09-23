42e513ff25dab53c1c58dfc58f200f96ec825d5e
expert7kr <expert7kr@naver.com>
2019-09-23 22:37:10 +0900

    #28 Coding DIY Bootstrap4 : webpack webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const fileOptions = {
+    'css': ['app.bundle.css'],
+    'js': ['app.bundle.js'],
+    'chunks': {
+        'head': {
+            'entry': '',
+            'css': 'app.bundle.css'
+        },
+        'body': {
+            'entry': 'app.bundle.js',
+            'css': ''
+        }
+    }
+}
 module.exports = {
     entry: './src/assets/js/app.js',
     output: {
@@ -18,10 +35,70 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/
             },
+            // {
+            //     test: /\.s?css$/,
+            //     use: ['style-loader', 'css-loader', 'sass-loader']
+            // },
             {
-                test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use:['css-loader', 'postcss-loader']
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use:['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader?name=assets/image/',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                              progressive: true,
+                              quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: [0.65, 0.90],
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                              quality: 75
+                            }
+                        }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './src/assets/images', to: './assets/images/'}
+        ]),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            'files': fileOptions
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'intro.html',
+            template: 'src/intro.html',
+            'files': fileOptions
+        }),
+        new ExtractTextPlugin('assets/css/app.bundle.css')
+    ]
 };
